50c9fca045a85159b704b27df4ab2bf8905af781
YangDrYang <37473872+YangDrYang@users.noreply.github.com>
2022-05-21 10:52:31 +0930

    cleaning

--- init_phifa.m
+++ init_phifa.m
@@ -1,28 +1,19 @@
 disp('Initialising PHiFA');
 
 disp('Adding path...');
+
 addpath('.');
 addpath('./thirdparty');
 addpath(genpath('propagator'));
-% addpath(genpath('functions'));
 addpath('./functions');
 addpath('./functions/ABM8Integrator');
 addpath(genpath('classes'));
 addpath(genpath('scripts'));
-addpath(genpath('matlab2tikz'));
 
 disp('Creating folders...');
 
-if ~exist('logfiles', 'dir')
-    mkdir('logfiles');
-end
-
 if ~exist('figures', 'dir')
     mkdir('figures');
-end
-
-if ~exist('stlfiles', 'dir')
-    mkdir('stlfiles');
 end
 
 disp('Welcome! Lets do this.');
